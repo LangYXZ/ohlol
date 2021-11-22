dba8a00160e50b460026113097d494088c9c111a
LangXYZ <thismynamelang@gmail.com>
2021-11-22 14:53:22 +0700

    aha

---
+++ database/menu.js
@@ -0,0 +1,317 @@
+const menunya = (prefix) => {
+	return`
+├━━֍ 「 Bot Info 」 ❒
+├ _Creator : Alanggzz
+├ _Nomor Creator : +6283892784397
+├ _Nama Bot : AlangzzBOTZ2
+├  Battery : ${baterai.battery}
+├ _Mode : ${publik ? 'Public' : 'Self'}
+├ _Prefix : ${multi ? 'Multi Prefix' : 'No Prefix'}
+├ _Total Hit : ${cmhit.length}
+├ _Fitur : +214
+└━━֍
+
+╭─❒  〔📌OWNER📌〕
+🏴‍☠️ .autorespon [ on/off ]
+🏴‍☠️ .bc [ teks/reply gif/image/video with caption ]
+🏴‍☠️ .tobc [ reply sticker/audio with caption ]
+🏴‍☠️ .delchat
+🏴‍☠️ .mute
+🏴‍☠️ .unmute
+🏴‍☠️ .public
+🏴‍☠️ .self
+🏴‍☠️ .spam [ teks|jumlah ]
+🏴‍☠️ .demoteall
+🏴‍☠️ .promoteall
+🏴‍☠️ .addcmd [ teks reply stc ]
+🏴‍☠️ .delcmd [ reply stc ]
+🏴‍☠️ .listcmd
+🏴‍☠️ .spamsw [ teks|jumlah ]
+🏴‍☠️ .upswteks [ teks ]
+🏴‍☠️ .upswlokasi [ teks ]
+🏴‍☠️ .upswaudio [ reply audio ]
+🏴‍☠️ .upswvoice [ reply audio ]
+🏴‍☠️ .upswsticker [ reply sticker ]
+🏴‍☠️ .upswimage [ reply image with caption ]
+🏴‍☠️ .upswgif [ reply gif with caption ]
+🏴‍☠️ .upswvideo [ reply video with caption ]
+🏴‍☠️ .shutdown
+🏴‍☠️ .offline [ alasan ]
+🏴‍☠️ .online
+🏴‍☠️ .exif [ nama|author ]
+🏴‍☠️ .setprofile [ reply image ]
+🏴‍☠️ .setname [ teks ]
+🏴‍☠️ .setprefix [ multi/nopref ]
+🏴‍☠️ .setbio [ teks ]
+🏴‍☠️ .leave
+🏴‍☠️ .restart
+🏴‍☠️ .join [ link group ]
+🏴‍☠️ .delrespon [ nama ]
+🏴‍☠️ .q [ reply message ]
+└❏
+
+╭─❒ 〔📌INFO OWNER📌〕
+🏴‍☠️ .igowner
+🏴‍☠️ .youtubeowner
+🏴‍☠️ .tiktokowner 
+🏴‍☠️ .pacarowner
+🏴‍☠️ .mantanowner
+🏴‍☠️ .biodataowner
+└❏ 
+
+╭─❒ 〔📌ISLAM📌〕
+🏴‍☠️ .asmaulhusna
+🏴‍☠️ .yasin
+🏴‍☠️ .surah al fatihah
+🏴‍☠️ .kisah nabi muhammad
+🏴‍☠️ .surah al baqarah
+🏴‍☠️ .tatacara mandi wajib
+🏴‍☠️ .jadwal sholat seluruh indonesia
+└❏ 
+
+╭─❒ 〔📌UPLOAD SW📌〕
+🏴‍☠️ .spamsw
+🏴‍☠️ .upswteks
+🏴‍☠️ .upswsticker
+🏴‍☠️ .upswaudio
+🏴‍☠️ .upswvoice
+🏴‍☠️ .upswvideo
+🏴‍☠️ .upswgif
+🏴‍☠️ .upswimage
+└❏ 
+  
+╭─❒ 〔📌GRUP📌〕
+🏴‍☠️ . welcome on/off
+🏴‍☠️ .ad ( reply pesan )
+🏴‍☠️ .getpict [ @tag ]
+🏴‍☠️ .getname [ reply target ]
+🏴‍☠️ .getbio [ reply target ]
+🏴‍☠️ .promote [ @tag ]
+🏴‍☠️ .demote [ @tag ]
+🏴‍☠️ .antilink [ 1/0 ]
+🏴‍☠️ .creategrup [ nama|@tag ]
+🏴‍☠️ .tictactoe [ @tag ]
+🏴‍☠️ .delttt
+🏴‍☠️ .getpp
+🏴‍☠️ .kick [ @tag ]
+🏴‍☠️ .add [ nomor ]
+🏴‍☠️ .kik [ Reply Pesan Target ]
+🏴‍☠️ .ad [ Reply Pesan Target ]
+🏴‍☠️ .getdeskgc
+🏴‍☠️ .sider [ reply pesan bot ]
+🏴‍☠️ .setnamegc [ teks ]
+🏴‍☠️ .setdeskgc [ teks ]
+🏴‍☠️ .fitnah [ @tag|teks1|teks2 ]
+🏴‍☠️ .kontak [ @tag|nama ]
+🏴‍☠️ .kontag [ @tag|nama ]
+🏴‍☠️ .opengc
+🏴‍☠️ .closegc
+🏴‍☠️ .resetlinkgc
+🏴‍☠️ .linkgrup
+🏴‍☠️ .listonline
+🏴‍☠️ .listadmin
+🏴‍☠️ .hidetag [ teks ]
+🏴‍☠️ .sticktag [ nama sticker ]
+🏴‍☠️ .totag [ reply media ]
+└❏ 
+
+╭─❒ 〔📌MAKER📌〕
+🏴‍☠️ .maker2d2
+🏴‍☠️ .maker2d3
+🏴‍☠️ .maker2d4
+🏴‍☠️ .transformer
+🏴‍☠️ .battlefield
+🏴‍☠️ .coffeecup
+🏴‍☠️ .coffeecup2
+🏴‍☠️ .neon
+🏴‍☠️ .glow
+🏴‍☠️ .summer
+🏴‍☠️ .flower
+🏴‍☠️ .burn
+🏴‍☠️ .quote
+🏴‍☠️ .wooden
+🏴‍☠️ .golden
+└❏ 
+
+╭─❒ 〔📌DOWNLOADER📌〕
+🏴‍☠️ .ytmp3 [ link yt ]
+🏴‍☠️ .ytsearch [ judul ]
+🏴‍☠️ .lirik [ judul ]
+🏴‍☠️ .ytdownload 
+└❏ 
+
+╭─❒  〔📌FUN📌〕
+🏴‍☠️ .bisakah
+🏴‍☠️ .cekpintar
+🏴‍☠️ .cekganteng
+🏴‍☠️ .cekcantik
+🏴‍☠️ .cekmati
+🏴‍☠️ .truth
+🏴‍☠️ .dare
+🏴‍☠️ .delttc
+🏴‍☠️ .fitnahpc
+🏴‍☠️ .hbd
+🏴‍☠️ .jadian
+🏴‍☠️ .slot
+🏴‍☠️ .suit
+🏴‍☠️ .tictactoe
+🏴‍☠️ .dadu
+🏴‍☠️ .caripesan [ teks|jumlah ]
+🏴‍☠️ .pantun
+🏴‍☠️ .gombalan
+🏴‍☠️ .katakata bijak
+🏴‍☠️ .katakata patrick
+🏴‍☠️ .katakata squidward
+🏴‍☠️ .katakata plankton
+🏴‍☠️ .kata lordfahri
+🏴‍☠️ .katakata tuankrab
+🏴‍☠️ .sadboy
+└❏ 
+
+╭─❒  〔📌Motivasi📌〕
+🏴‍☠️ .cowok
+🏴‍☠️ .cewek
+🏴‍☠️ .lonte
+🏴‍☠️ .duda
+🏴‍☠️ .janda
+🏴‍☠️ .banci
+🏴‍☠️ .gay
+└❏ 
+
+╭─❒  〔📌Quotes📌〕
+🏴‍☠️ .quotesdilan
+🏴‍☠️ .quotesanime
+🏴‍☠️ .quotesnolep
+🏴‍☠️ .quoteswibu
+🏴‍☠️ .quotesbucin
+└❏ 
+
+╭─❒  〔📌STICKER📌〕
+🏴‍☠️ .sticker
+🏴‍☠️ .takestick
+🏴‍☠️ .colong
+└❏ 
+
+╭─❒  〔📌SEARCH📌〕
+🏴‍☠️ .ytmp3 [ link yt ]
+🏴‍☠️ .ytsearch [ judul ]
+🏴‍☠️ .lirik [ judul ]
+🏴‍☠️ .herodetail [ hero ]
+🏴‍☠️ .herolist
+└❏ 
+
+╭─❒  〔📌WAR📌〕
+🏴‍☠️ .xbug
+🏴‍☠️ .bugbutton
+🏴‍☠️ .bugrow
+🏴‍☠️ .bugcombine
+🏴‍☠️ .tesbug
+🏴‍☠️ .bugpc
+🏴‍☠️ .bugtroli2
+🏴‍☠️ .bugtroli3
+🏴‍☠️ .virtex
+🏴‍☠️ .tovirvid
+🏴‍☠️ .tovirgam
+🏴‍☠️ .bugkatalog (perbaikan)
+🏴‍☠️ .bugloc
+🏴‍☠️ .buglokasi
+🏴‍☠️ .x
+└❏ 
+
+╭─❒  〔📌STORAGE📌〕
+🏴‍☠️ .addimage
+🏴‍☠️ .addsticker
+🏴‍☠️ .addvn
+🏴‍☠️ .delimage
+🏴‍☠️ .delsticker
+🏴‍☠️ .delvn
+🏴‍☠️ .liststicker
+🏴‍☠️ .listvn
+🏴‍☠️ .listimage
+└❏ 
+
+╭─❒  〔📌ASUPAN📌〕
+🏴‍☠️ .cecanrandom
+🏴‍☠️ .cecanindonesia
+🏴‍☠️ .cecanvietnam
+🏴‍☠️ .cecanmalaysia
+🏴‍☠️ .cecanthailand
+🏴‍☠️ .hijaber
+🏴‍☠️ .ukhti ( video )
+🏴‍☠️ .asupan (video)
+🏴‍☠️ .ghea (video)
+🏴‍☠️ .rika (video)
+└❏ 
+
+╭─❒  〔📌Menu Dewasa📌〕
+🏴‍☠️ .dewasa1
+🏴‍☠️ .dewasa2
+🏴‍☠️ .dewasa3
+🏴‍☠️ .dewasa4
+🏴‍☠️ .dewasa5
+🏴‍☠️ .dewasa6
+🏴‍☠️ .dewasa7
+🏴‍☠️ .dewasa8
+🏴‍☠️ .dewasa9
+🏴‍☠️ .dewasa10
+🏴‍☠️ .dewasa11
+🏴‍☠️ .dewasa12
+🏴‍☠️ .dewasa13
+🏴‍☠️ .dewasa14
+🏴‍☠️ .dewasa15
+🏴‍☠️ .dewasa16
+🏴‍☠️ .dewasa17
+└❏ 
+
+╭─❒  〔📌Menu JavDewasa📌〕
+🏴‍☠️ .javmilf
+🏴‍☠️ .javoneesan
+🏴‍☠️ .javgangbang
+🏴‍☠️ .japaneimifukada
+🏴‍☠️ .japanntr
+🏴‍☠️ .japanschool
+🏴‍☠️ .japancosplay
+🏴‍☠️ .japanteacher
+└❏ 
+
+╭─❒ 〔📌Hentai📌〕
+🏴‍☠️ .HentaiMilf
+🏴‍☠️ .HentaiOneesan
+🏴‍☠️ .HentaiLoli
+🏴‍☠️ .Hentaigangbang
+🏴‍☠️ .HentaiNtr
+🏴‍☠️ .HentaiIncest
+🏴‍☠️ .HentaiVillain
+🏴‍☠️ .HentaiYuri
+🏴‍☠️ .HentaiYaoi
+└❏ 
+
+╭─❒  〔📌TENTANG BOT📌〕
+🏴‍☠️ .listblock
+🏴‍☠️ .getcaption
+🏴‍☠️ .listgrup
+🏴‍☠️ .owner
+🏴‍☠️ .report (Pesan Errornya)
+🏴‍☠️ .rules
+🏴‍☠️ .speed
+🏴‍☠️ .status
+🏴‍☠️ .tes
+└❏ 
+
+╭─❒  〔📌Bot📌〕
+🏴‍☠️ .jadibot
+🏴‍☠️ .stopjadibot
+└❏ 
+
+├━━֍  〔📌CONVERT📌〕
+🏴‍☠️ .balik (reply audio)
+🏴‍☠️ .bass (reply audio)
+🏴‍☠️ .detikvn (reply audio)
+🏴‍☠️ .detikvideo (reply audio)
+🏴‍☠️ .gemuk (reply audio)
+🏴‍☠️ .robot (reply audio)
+🏴‍☠️ .tomp3 [ reply video ]
+🏴‍☠️ .toimg [reply stiker]
+└❏ 〔📌${NamaBot}📌〕`
+}
+exports.menunya = menunya
